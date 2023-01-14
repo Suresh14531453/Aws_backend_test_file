ba946af90bb957f6473926d3b09b8053741b2024
sureshsahu <suresh.sahu@trangile.com>
2023-01-14 23:40:41 +0530

    delete

--- app.js
+++
@@ -1,8 +0,0 @@
-const express=require("express")
-const app=express();
-app.get("/",(req,res)=>{
-    res.send("hit to all")
-})
-app.listen(9090,()=>{
-    console.log("app listening at 9090 is running")
-})
