0b37a60014a53e1680ee2e2fefcbf3436d502a1f
sureshsahu <suresh.sahu@trangile.com>
2023-01-14 19:16:11 +0530

    remove

--- app.js
+++
@@ -1,8 +0,0 @@
-const express=require("express");
-const app=express();
-app.get("/",(req,res)=>{
-    res.send("its working properly")
-})
-app.listen(8080,()=>{
-    console.log("server listening at 8080")
-})
